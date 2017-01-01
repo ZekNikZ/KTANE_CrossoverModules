33673643021747beba06d2918cc66ae5eede0bf1
Matthew McCaskill <GamrCorps@gmail.com>
2017-01-01 12:42:20 -0700

    Fixed the .gitignore file accidentally excluding some files from the html manual.

---
+++ Assets/Modules/Symbolic Password/SymbolicPasswordModule.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SymbolicPasswordModule : MonoBehaviour {
+
+	public KMBombInfo BombInfo;
+	public KMBombModule BombModule;
+	public KMAudio Audio;
+	public KMSelectable[] buttons;
+	public KMSelectable submitButton;
+	public MeshRenderer[] labels;
+	public Material[] symbols;
+	public Material blankScreen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
